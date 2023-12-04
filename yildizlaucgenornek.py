ad2cf33fc671e2bdbeb90de6da328f04d7a2609c
Musa Bayar <148579016+musabayar@users.noreply.github.com>
2023-12-05 02:22:48 +0300

    Add files via upload

---
+++ yildizlaucgenornek.py
@@ -0,0 +1,9 @@
+for k in range(0,10,1):
+    for i in range(k+1):
+        print('*', end=' ')
+    print()
+
+for k in range(10,0,-1):
+    for i in range(k):
+        print('*', end=' ')
+    print()
